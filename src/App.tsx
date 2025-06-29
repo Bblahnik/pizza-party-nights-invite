d2d22daccc55090d88249d4f366680fdca1fa241
Brook Blahnik <blahnik@umn.edu>
2025-06-29 13:58:21 -0500

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/pizza-party-nights-invite">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
